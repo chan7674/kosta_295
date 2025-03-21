3c2a86008bdbd5207bf0e4e7efdb72e167df874a
chan7674 <148619047+chan7674@users.noreply.github.com>
2025-03-21 16:38:29 +0900

    Update OrderService.java

--- src/exam/order/OrderService.java
+++ src/exam/order/OrderService.java
@@ -5,6 +5,8 @@ public class OrderService {
 	public void insert() {
 		System.out.println("bug/order 브랜치에서 작업함.");
 
+	}
+
   public void select() {
 		System.out.println("bug/order 원격 브랜치에서 작업함.");
 
