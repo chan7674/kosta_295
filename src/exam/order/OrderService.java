8a0c2d622db89c0727c86720eb4b2fbe8764b009
chan7674 <skc3924@gmail.com>
2025-03-21 16:26:13 +0900

    t

--- src/exam/order/OrderService.java
+++ src/exam/order/OrderService.java
@@ -4,8 +4,9 @@ public class OrderService {
 
 	public void insert() {
 		System.out.println("bug/order 브랜치에서 작업함.");
+	}
 
-  public void select() {
+	public void select() {
 		System.out.println("bug/order 원격 브랜치에서 작업함.");
 
 	}
